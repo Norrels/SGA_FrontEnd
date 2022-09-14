f010845ad1ce6008eb322c9c338600d41a5020fd
Arthur-Rosa <arthur.corporativo@outlook.com>
2022-09-14 19:16:18 -0300

    :lipstick: style course

---
+++ src/pages/Course/components/CourseItem/style.ts
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const CourseItemContainer = styled.div`
+    width: 100%;
+    padding: 2rem;
+    margin-bottom: 1rem;
+
+    border-radius: 8px;
+    background: ${(props) => props.theme["white"]};
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+export const CourseItemInfoContent = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 3rem;
+
+    img, svg {
+        width: 40px;
+        height: 40px;
+        border-radius: 8px;
+    }
+
+    span {
+        font-weight: bold;
+    }
+`
+
+export const CourseInfoType = styled.div`
+    font-size: 0.800rem;
+    width: 100px;
+    padding: 5px;
+
+    background: ${(props) => props.theme["blue-200"]};
+    text-align: center;
+    border-radius: 1rem;
+    font-weight: bold;
+`
+
+export const CourseDescription = styled.div``
+
+const BUTTONS = {
+    edit: "blue-200",
+    delete: "blue-400",
+  } as const;
+  
+  interface ButtonProps {
+    buttonColor: keyof typeof BUTTONS;
+  }
+  
+  export const CourseItemButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+  `;
+  
+  export const CourseItemButton = styled.button<ButtonProps>`
+    border: none;
+    border-radius: 8px;
+    padding: 0.2rem;
+  
+    background: ${(props) => props.theme[BUTTONS[props.buttonColor]]};
+  
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
--- src/pages/Course/index.tsx
+++ src/pages/Course/index.tsx
@@ -1,5 +1,33 @@
+import { CourseItem } from "./components/CourseItem";
+import {
+  CourseButtonContainer,
+  CourseContainer,
+  CourseContent,
+  CourseList,
+  CourseTitleContainer,
+} from "./style";
+
 export function Course() {
   return (
-    <h1>Cursos</h1>
-  )
+    <CourseContainer>
+      <CourseContent>
+        <CourseTitleContainer>
+          <h1>Cursos</h1>
+          <p>Selecione um curso ou crie um novo!</p>
+          <CourseButtonContainer>
+            <button>Novo Curso</button>
+          </CourseButtonContainer>
+        </CourseTitleContainer>
+        <input type="text" placeholder="Buscar por Curso" />
+
+        <CourseList>
+          <CourseItem />
+          <CourseItem />
+          <CourseItem />
+          <CourseItem />
+          <CourseItem />
+        </CourseList>
+      </CourseContent>
+    </CourseContainer>
+  );
 }
