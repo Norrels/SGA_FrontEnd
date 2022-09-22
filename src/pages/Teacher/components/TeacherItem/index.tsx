6b1532f887be6aad44d3b37c40bb89d9e9fca582
Arthur-Rosa <arthur.corporativo@outlook.com>
2022-09-21 22:26:30 -0300

    :construction: Modal Editar Professores

--- src/pages/Teacher/components/TeacherItem/index.tsx
+++ src/pages/Teacher/components/TeacherItem/index.tsx
@@ -1,27 +1,81 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import { DotsThree, Trash } from "phosphor-react";
-import { TeacherItemButton, TeacherItemButtonContainer, TeacherItemContainer, TeacherItemInfoContainer, TeacherItemInfoContent,  } from "./style";
+import { EditTeacherModal } from "../EditTeacherModal";
+import {
+  TeacherItemButton,
+  TeacherItemButtonContainer,
+  TeacherItemContainer,
+  TeacherItemInfoContainer,
+  TeacherItemInfoContent,
+} from "./style";
 
-export function TeacherItem() {
+interface Teacher {
+  id?: number;
+  nome?: string;
+  cargaSemanal?: number;
+  ausencia?: string;
+  url?: string;
+  listaCompetencia?: listaCompetencia[];
+}
+
+interface listaCompetencia {
+  id?: number;
+  unidadeCurricular?: unidadeCurricular;
+  nivelHabilidade?: string;
+}
+
+interface unidadeCurricular {
+  id?: number;
+  nome?: string;
+  horas?: number;
+  curso?: curso;
+}
+
+interface curso {
+  id?: number;
+  nome?: string;
+  tipoCurso?: string;
+}
+
+export function TeacherItem({
+  id,
+  nome,
+  cargaSemanal,
+  ausencia,
+  url,
+  listaCompetencia,
+}: Teacher) {
   return (
     <TeacherItemContainer>
       <TeacherItemInfoContainer>
-        <img
-          src="https://avatars.githubusercontent.com/u/54322854?v=4"
-          alt=""
-        />
+        <img src={url} alt="" />
 
         <TeacherItemInfoContent>
-          <h3>Rafaela Balarini</h3>
-          <p>Carga horária: 40hs</p>
+          <h3>{nome}</h3>
+          <p>Carga horária: {cargaSemanal}hs</p>
         </TeacherItemInfoContent>
       </TeacherItemInfoContainer>
 
       <TeacherItemButtonContainer>
-        <TeacherItemButton buttonColor="edit">
-          <DotsThree color="#000" size={25} />
-        </TeacherItemButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <TeacherItemButton buttonColor="edit">
+              <DotsThree color="#000" size={25} />
+            </TeacherItemButton>
+          </Dialog.Trigger>
+          <EditTeacherModal
+            key={id}
+            listaCompetencia={listaCompetencia}
+            id={id}
+            nome={nome}
+            cargaSemanal={cargaSemanal}
+            ausencia={ausencia}
+            url={url}
+          />
+        </Dialog.Root>
+
         <TeacherItemButton buttonColor="delete">
-          <Trash color="#fff" size={25}/>
+          <Trash color="#fff" size={25} />
         </TeacherItemButton>
       </TeacherItemButtonContainer>
     </TeacherItemContainer>
