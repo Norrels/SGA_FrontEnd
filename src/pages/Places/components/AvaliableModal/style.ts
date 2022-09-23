08729dbd57898c2d1d6c82f81a9e85044a0c447f
Arthur-Rosa <arthur.corporativo@outlook.com>
2022-09-23 19:07:24 -0300

    :recycle: refactor: Tela de Disponibilidade de Ambientes

--- src/pages/Places/components/AvaliableModal/style.ts
+++ src/pages/Places/components/AvaliableModal/style.ts
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import styled from "styled-components";
+import * as Checkbox from "@radix-ui/react-checkbox";
 
 export const Overlay = styled(Dialog.Overlay)`
   width: 100w;
@@ -9,6 +10,49 @@ export const Overlay = styled(Dialog.Overlay)`
   inset: 0;
 
   background: rgba(0, 0, 0, 0.75);
+`;
+
+export const ModalCreateClassDays = styled.article`
+  display: grid !important;
+  grid-template-columns: repeat(7, 1fr) !important;
+
+  span {
+    display: flex !important;
+    flex-direction: column !important;
+    align-items: center !important;
+
+    input:checked {
+      button {
+        background-color: ${(props) => props.theme["black"]} !important;
+      }
+    }
+  }
+`;
+
+export const HomeCheckBox = styled(Checkbox.Root)`
+  background-color: ${(props) => props.theme["white"]};
+  height: 2.125rem !important;
+  width: 2.125rem !important;
+  border: none !important;
+
+  :checked {
+    background-color: azure !important;
+    accent-color: black !important;
+  }
+
+  border-radius: 999999px !important;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25) !important;
+`;
+
+export const HomeCheckBoxIndicator = styled(Checkbox.Indicator)`
+  display: flex !important;
+  align-items: center !important;
+  justify-content: center !important;
+
+  :checked {
+    background-color: azure !important;
+    accent-color: black !important;
+  }
 `;
 
 export const Content = styled(Dialog.Content)`
@@ -41,12 +85,12 @@ export const CloseButton = styled(Dialog.Close)`
 `;
 
 export const AvaliableModalContainer = styled.form`
-  margin-top: 3rem;
+  margin-top: 1rem;
 
   button {
     width: 100%;
     height: 3rem;
-    margin-top: 2rem;
+    margin-top: 1rem;
 
     border: none;
     border-radius: 8px;
@@ -112,11 +156,124 @@ export const AvaliableModalContent = styled.div`
 
 export const AvaliableModalDivider = styled.span`
   margin-top: 0.6rem;
-  
+
   font-size: 0.938rem;
   font-weight: 500;
   color: ${(props) => props.theme["gray-700"]};
 
   display: flex;
   justify-content: center;
+`;
+
+export const TableScroow = styled.div`
+  width: 100%;
+  overflow: auto;
+  border-radius: 8px;
+  box-shadow: 0px 4px 10px 3px rgba(0, 0, 0, 0.1);
+  background: transparent;
+  height: 150px;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+    position: absolute;
+  }
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0.1);
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: rgba(0, 0, 0, 0.2);
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: rgba(0, 0, 0, 0.4);
+  }
+  ::-webkit-scrollbar-thumb:active {
+    background: rgba(0, 0, 0, 0.9);
+  }
+`;
+
+export const AdvancedTableContent = styled.main`
+  display: flex;
+  gap: 2.625rem;
+
+  aside {
+    width: 25%;
+    border-radius: 8px;
+    padding: 1.875rem 1.25rem;
+    background-color: ${(props) => props.theme["white"]};
+    overflow: auto;
+    box-shadow: 0px 4px 10px 3px rgba(0, 0, 0, 0.1);
+
+    ::-webkit-scrollbar {
+      width: 6px;
+      height: 6px;
+    }
+    ::-webkit-scrollbar-track {
+      border-radius: 10px;
+      background: rgba(0, 0, 0, 0.1);
+    }
+    ::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      background: rgba(0, 0, 0, 0.2);
+    }
+    ::-webkit-scrollbar-thumb:hover {
+      background: rgba(0, 0, 0, 0.4);
+    }
+    ::-webkit-scrollbar-thumb:active {
+      background: rgba(0, 0, 0, 0.9);
+    }
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    height: 50;
+    border-radius: 8px;
+  }
+
+  tbody {
+    height: 10px;
+    overflow: auto;
+    box-shadow: 0px 4px 10px 3px rgba(0, 0, 0, 0.1);
+  }
+
+  thead th {
+    height: 4.063rem;
+    position: sticky;
+    top: 0;
+    color: ${(props) => props.theme["white"]};
+
+    background: linear-gradient(180deg, #5aadd1 0%, #367fbf 100%);
+
+    &:first-child {
+      border-radius: 8px 0px 0px 0px;
+    }
+
+    &:last-child {
+      border-radius: 0px 8px 0px 0px;
+    }
+  }
+
+  tr {
+    height: 3.438rem;
+
+    background: ${(props) => props.theme["white"]};
+
+    :nth-child(2n) {
+      background: ${(props) => props.theme["white-200"]};
+    }
+
+    svg {
+      cursor: pointer;
+    }
+  }
+
+  td {
+    vertical-align: middle;
+    text-align: center;
+    font-weight: 500;
+  }
 `;
