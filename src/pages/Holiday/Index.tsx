15569469c666dcadb49520c2844aa48a9f80896b
Arthur-Rosa <arthur.corporativo@outlook.com>
2022-09-22 21:55:57 -0300

    :sparkles: feat: Nova Modal editar Dia

--- src/pages/Holiday/Index.tsx
+++ src/pages/Holiday/Index.tsx
@@ -1,10 +1,26 @@
-import * as Dialog from '@radix-ui/react-dialog'
-import React from 'react'
-import { HolidayItem } from './components/HolidayItem'
-import { NewHolidayModal } from './components/NewHolidayModal'
-import { HolidayButtonContainer, HolidayContainer, HolidayContent, HolidayList, HolidayTitleContainer } from './style'
+import * as Dialog from "@radix-ui/react-dialog";
+import React from "react";
+import { HolidayItem } from "./components/HolidayItem";
+import { NewHolidayModal } from "./components/NewHolidayModal";
+import {
+  HolidayButtonContainer,
+  HolidayContainer,
+  HolidayContent,
+  HolidayList,
+  HolidayTitleContainer,
+} from "./style";
 
 export function Holiday() {
+  const holidays = [
+    {
+      id: 2,
+      dataInicio: "2022-10-10",
+      dataFinal: "",
+      nome: "Dia das Crianças",
+      tipoDeDia: "FERIADO",
+    },
+  ];
+
   return (
     <HolidayContainer>
       <HolidayContent>
@@ -23,13 +39,18 @@ export function Holiday() {
         <input type="text" placeholder="Buscar um dia não letivo" />
 
         <HolidayList>
-          <HolidayItem />
-          <HolidayItem />
-          <HolidayItem />
-          <HolidayItem />
-          <HolidayItem />
+          {holidays.map((v) => (
+            <HolidayItem
+              key={v.id}
+              id={v.id}
+              dataFinal={v.dataFinal}
+              dataInicio={v.dataInicio}
+              nome={v.nome}
+              tipoDeDia={v.tipoDeDia}
+            />
+          ))}
         </HolidayList>
       </HolidayContent>
     </HolidayContainer>
-  )
+  );
 }
