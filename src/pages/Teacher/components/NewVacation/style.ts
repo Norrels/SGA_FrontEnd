723ed24155cc40ba5da05cb71a34656b408ae120
bruno <bruno.duarte314@gmail.com>
2022-10-05 15:06:40 -0300

    :construction: feat: Continuando logica da modal de férias

--- src/pages/Teacher/components/NewVacation/style.ts
+++ src/pages/Teacher/components/NewVacation/style.ts
@@ -123,35 +123,6 @@ export const InputIndividual = styled.div`
   }
 `;
 
-export const CheckContainer = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  margin: 1.25rem 0;
-`;
-
-export const CheckContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.313rem;
-`;
-
-export const CheckIndividual = styled.div`
-  display: flex;
-  align-items: center;
-  label {
-    font-weight: 300;
-    font-size: 1rem;
-    color: rgba(0, 0, 0, 0.4);
-    margin: 0 5px;
-  }
-
-  input {
-    width: 20px;
-    height: 20px;
-  }
-`;
-
 export const FinalButton = styled.div`
   width: 100%;
   button {
