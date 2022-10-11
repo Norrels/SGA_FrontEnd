7a5d866d02da8671bbaebb33f04c2fd0ecc6ffd1
Arthur-Rosa <arthur.corporativo@outlook.com>
2022-10-11 19:37:33 -0300

    :sparkles: feat: new autocomplete admin

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { API } from "../../lib/axios";
 import { AdminItem } from "./components/AdminItem";
 import { NewAdminModal } from "./components/NewAdminModal";
@@ -19,22 +19,35 @@ export interface AdminProps {
   tipoCurso: string;
   senha: string;
   ativo: string;
-}[]
+}
+[];
 
 export function Admin() {
-  
   const [admin, setAdmin] = useState<AdminProps[]>([]);
+  const [adminMatches, setAdminMatches] = useState<AdminProps[]>([]);
 
   async function fetchAdmin() {
-    const res = await API.get("usuario")
-    
-    console.log(res.data)
+    const res = await API.get("usuario");
+
+    console.log(res.data);
     setAdmin(res.data);
   }
 
   useEffect(() => {
     fetchAdmin();
   }, []);
+
+  const searchAdmin = (text: String) => {
+    if (!text) {
+      setAdminMatches([]);
+    } else {
+      let matches = admin.filter((admin) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return admin.nome.match(regex) || admin.nif.match(regex);
+      });
+      setAdminMatches(matches);
+    }
+  };
 
   return (
     <AdminContainer>
@@ -51,12 +64,17 @@ export function Admin() {
             </Dialog.Root>
           </AdminButtonContainer>
         </AdminTitleContainer>
-        <input type="text" placeholder="Buscar por Ambiente" />
-
+        <input
+          type="text"
+          placeholder="Buscar por Ambiente"
+          onChange={(e) => searchAdmin(e.target.value)}
+        />
         <AdminList>
-          {admin.map((admin) => (
-            <AdminItem key={admin.id} admin={admin} />
-          ))}
+          {adminMatches.length == 0
+            ? admin.map((admin) => <AdminItem key={admin.id} admin={admin} />)
+            : adminMatches.map((admin) => (
+                <AdminItem key={admin.id} admin={admin} />
+              ))}
         </AdminList>
       </AdminContent>
     </AdminContainer>
