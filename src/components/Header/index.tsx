394a124332162c0b7e408d4c49b424614a567556
Matheus Zacarias <matheuus412@gmail.com>
2022-10-17 21:56:27 -0300

    :bug: fix: react-remove-scroll-bar: cannot calculate scrollbar size because it is removed (overflow:hidden on body

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -25,80 +25,92 @@ export function Header() {
     setOpen(false);
   }
 
-  return (
-    <HeaderContainer>
-      <HeaderContent>
-        <img src={Logo} alt="" />
+  function closeAulas() {
+    setTimeout(() => {
+      setOpen(true);
+    }, 5)
+  }
 
-        <HeaderNavBar>
-          <HeaderNavMenu>
-            <NavLink to="/aulas" title="Início">
-              Início
+  return (
+    <>
+      <HeaderContainer>
+        <HeaderContent>
+          <img src={Logo} alt="" />
+
+          <HeaderNavBar>
+            <HeaderNavMenu>
+              <NavLink to="/aulas" title="Início">
+                Início
+              </NavLink>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger asChild>
+                  <CaretDown weight="fill" />
+                </DropdownMenu.Trigger>
+                <HeaderNavMenuContent>
+                  <HeaderNavMenuArrow>
+                    <CaretUp weight="fill" size={30} />
+                  </HeaderNavMenuArrow>
+
+                  <HeaderNavMenuItem>
+                    <NavLink to="/busca-avancada">Aulas</NavLink>
+                  </HeaderNavMenuItem>
+                  <HeaderNavMenuItem>
+                    <NavLink to="/dias-nao-letivos">Dias não letivo</NavLink>
+                  </HeaderNavMenuItem>
+                </HeaderNavMenuContent>
+              </DropdownMenu.Root>
+            </HeaderNavMenu>
+            <NavLink to="/dashboard" title="Dashboard">
+              Dashboard
             </NavLink>
+            <HeaderNavMenu>
+              <NavLink to="/professores" title="Professor">
+                Professores
+              </NavLink>
+            </HeaderNavMenu>
+            <NavLink to="/cursos" title="Cursos">
+              Cursos
+            </NavLink>
+            <NavLink to="/ambientes" title="Ambientes">
+              Ambientes
+            </NavLink>
+          </HeaderNavBar>
+
+          <HeaderUser>
+            <User size={23} />
+            <p>Odair</p>
             <DropdownMenu.Root>
               <DropdownMenu.Trigger asChild>
                 <CaretDown weight="fill" />
               </DropdownMenu.Trigger>
+
               <HeaderNavMenuContent>
                 <HeaderNavMenuArrow>
                   <CaretUp weight="fill" size={30} />
                 </HeaderNavMenuArrow>
 
-                <HeaderNavMenuItem>
-                  <NavLink to="/busca-avancada">Aulas</NavLink>
+                <HeaderNavMenuItem onClick={closeAulas}>
+                  <HeaderEditUserButton>Editar Perfil</HeaderEditUserButton>
+
                 </HeaderNavMenuItem>
                 <HeaderNavMenuItem>
-                  <NavLink to="/dias-nao-letivos">Dias não letivo</NavLink>
+                  <HeaderEditUserButton
+                    onClick={() => navigate("/", { replace: true })}
+                  >
+                    Sair <SignOut />
+                  </HeaderEditUserButton>
                 </HeaderNavMenuItem>
               </HeaderNavMenuContent>
             </DropdownMenu.Root>
-          </HeaderNavMenu>
-          <NavLink to="/dashboard" title="Dashboard">
-            Dashboard
-          </NavLink>
-          <HeaderNavMenu>
-            <NavLink to="/professores" title="Professor">
-              Professores
-            </NavLink>
-          </HeaderNavMenu>
-          <NavLink to="/cursos" title="Cursos">
-            Cursos
-          </NavLink>
-          <NavLink to="/ambientes" title="Ambientes">
-            Ambientes
-          </NavLink>
-        </HeaderNavBar>
+          </HeaderUser>
+        </HeaderContent>
+      </HeaderContainer>
 
-        <HeaderUser>
-          <User size={23} />
-          <p>Odair</p>
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger asChild>
-              <CaretDown weight="fill" />
-            </DropdownMenu.Trigger>
+      <Dialog.Root open={open} onOpenChange={setOpen}>
 
-            <HeaderNavMenuContent>
-              <HeaderNavMenuArrow>
-                <CaretUp weight="fill" size={30} />
-              </HeaderNavMenuArrow>
-
-              <HeaderNavMenuItem as={Dialog.Root} open={open} onOpenChange={setOpen}>
-                <Dialog.Trigger asChild>
-                  <HeaderEditUserButton>Editar Perfil</HeaderEditUserButton>
-                </Dialog.Trigger>
-                <EditUserModal closeModal={closeModal}/>
-              </HeaderNavMenuItem>
-              <HeaderNavMenuItem>
-                <HeaderEditUserButton
-                  onClick={() => navigate("/", { replace: true })}
-                >
-                  Sair <SignOut />
-                </HeaderEditUserButton>
-              </HeaderNavMenuItem>
-            </HeaderNavMenuContent>
-          </DropdownMenu.Root>
-        </HeaderUser>
-      </HeaderContent>
-    </HeaderContainer>
+        <EditUserModal closeModal={closeModal} />
+      </Dialog.Root>
+    </>
   );
+
 }
