02d637ef04d7512cc678ad0f25b460cb424342ce
Arthur-Rosa <arthur.corporativo@outlook.com>
2022-10-18 22:22:39 -0300

    :sparkles: feat: new modal aulas

--- src/pages/Home/components/ModalCreateNewClass/index.tsx
+++ src/pages/Home/components/ModalCreateNewClass/index.tsx
@@ -2,8 +2,10 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { Check, X } from "phosphor-react";
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+import { boolean, number, z } from "zod";
 import Resumo from "../../../../assets/Resumo.svg";
 import { ObjectsContext } from "../../../../Contexts/ObjectsContext";
+import { API } from "../../../../lib/axios";
 import {
   CloseButton,
   Content,
@@ -19,29 +21,90 @@ import {
   Overlay,
 } from "./style";
 
+export const aulaInput = z.object({
+  codTurma: z.string(),
+  periodo: z.string(),
+  dataInicio: z.date(),
+  cargaDiaria: z.string(),
+  diaSemana: z.boolean().array(),
+  curso: z.object({
+    id: z.string(),
+  }),
+  unidadeCurricular: z.object({
+    id: z.number(),
+  }),
+  professor: z.object({
+    id: z.number(),
+  }),
+  ambiente: z.object({
+    id: z.number(),
+  }),
+  semana: z.object({
+    segunda: z.object({
+      value: z.string(),
+    }),
+    terca: z.object({
+      value: z.string(),
+    }),
+    quarta: z.object({
+      value: z.string(),
+    }),
+    quinta: z.object({
+      value: z.string(),
+    }),
+    sexta: z.object({
+      value: z.string(),
+    }),
+    sabado: z.object({
+      value: z.string(),
+    }),
+    domingo: z.object({
+      value: z.string(),
+    }),
+  })
+});
+
+export type AulaType = z.infer<typeof aulaInput>;
 
 interface ModalCreateNewClassProps {
   name: string;
-  closeModal(): void
+  closeModal(): void;
 }
 
-export function ModalCreateNewClass({ name, closeModal }: ModalCreateNewClassProps) {
+interface SemanaProps {
+  id: number;
+  ativo: string;
+}
+[];
 
-  const { courses, placesList, curricularUnit } = useContext(ObjectsContext)
-  const { register, handleSubmit, reset } = useForm()
+export function ModalCreateNewClass({
+  name,
+  closeModal,
+}: ModalCreateNewClassProps) {
+  const { courses, placesList, curricularUnit, teachers } =
+    useContext(ObjectsContext);
+  const { register, handleSubmit, reset } = useForm<AulaType>();
+
+  const [semanaDays, setSemanaDays] = useState<SemanaProps[]>([]);
+
+  async function handleCreateAulaAPI(aula: AulaType) {
+    console.log(aula);
+    // const res = await API.post(`aula`, aula);
+  }
 
   const courseFiltedByType = courses.filter((course) => {
-    if (course.tipoCurso.toLowerCase() == name.toLowerCase()){
-      return course
-    } 
-    if(name == 'customizável'){
-      return course
+    if (course.tipoCurso.toLowerCase() == name.toLowerCase()) {
+      return course;
     }
-  })
+    if (name == "customizável") {
+      return course;
+    }
+  });
 
-  async function handleCreateNewPlace() {
-    reset()
-    closeModal()
+  async function handleCreateNewAula(data: AulaType) {
+    handleCreateAulaAPI(data);
+    reset();
+    closeModal();
   }
 
   return (
@@ -53,39 +116,46 @@ export function ModalCreateNewClass({ name, closeModal }: ModalCreateNewClassPro
         </CloseButton>
 
         <Dialog.Title>Aula {name}</Dialog.Title>
-        <ModalCreateClassContent onSubmit={handleSubmit(handleCreateNewPlace)}>
+        <ModalCreateClassContent onSubmit={handleSubmit(handleCreateNewAula)}>
           <ModalCreateClassContentLine>
             <label htmlFor="">Curso</label>
-            <select name="" id="">
+            <select {...register("curso.id")} name="" id="">
               {courseFiltedByType.map((course) => {
                 return (
-                  <option key={course.id} value="">{course.nome}</option>
-                )
+                  <option key={course.id} value={course.id}>
+                    {course.nome}
+                  </option>
+                );
               })}
-
             </select>
           </ModalCreateClassContentLine>
 
           <ModalCreateClassContentLine>
             <label htmlFor="">Unidade Curricular</label>
-            <select name="" id="">
+            <select {...register("unidadeCurricular.id")} name="" id="">
               {curricularUnit.map((unidade) => {
                 return (
-                  <option key={unidade.id} value="">{unidade.nome}</option>
-                )
+                  <option key={unidade.id} value={unidade.id}>
+                    {unidade.nome}
+                  </option>
+                );
               })}
             </select>
           </ModalCreateClassContentLine>
 
           <ModalCreateClassContentLine>
             <label htmlFor="">Código da turma</label>
-            <input type="text" placeholder="Digite o código da turma" />
+            <input
+              type="text"
+              {...register("codTurma")}
+              placeholder="Digite o código da turma"
+            />
           </ModalCreateClassContentLine>
 
           <ModalCreateClassContentLines>
             <ModalCreateClassContentCollum>
               <label htmlFor="">Periodo</label>
-              <select name="" id="">
+              <select {...register("periodo")} name="" id="">
                 <option value="MANHA">Manhã</option>
                 <option value="TARDE">Tarde</option>
                 <option value="NOITE">Noite</option>
@@ -94,14 +164,14 @@ export function ModalCreateNewClass({ name, closeModal }: ModalCreateNewClassPro
             </ModalCreateClassContentCollum>
             <ModalCreateClassContentCollum>
               <label htmlFor="">Data inicio</label>
-              <input type="date" />
+              <input type="date" {...register("dataInicio")} />
             </ModalCreateClassContentCollum>
           </ModalCreateClassContentLines>
 
           <ModalCreateClassDays>
-            <span onClick={() => console.log(courses)}>
+            <span>
               <label>Dom</label>
-              <HomeCheckBox>
+              <HomeCheckBox {...register("semana.domingo.value")}>
                 <HomeCheckBoxIndicator>
                   <Check size={20} weight="bold" color="#fff" />
                 </HomeCheckBoxIndicator>
@@ -109,7 +179,7 @@ export function ModalCreateNewClass({ name, closeModal }: ModalCreateNewClassPro
             </span>
             <span>
               <label>Seg</label>
-              <HomeCheckBox>
+              <HomeCheckBox {...register("semana.segunda.value")}>
                 <HomeCheckBoxIndicator>
                   <Check size={20} weight="bold" color="#fff" />
                 </HomeCheckBoxIndicator>
@@ -117,40 +187,40 @@ export function ModalCreateNewClass({ name, closeModal }: ModalCreateNewClassPro
             </span>
             <span>
               <label>Ter</label>
-              <HomeCheckBox>
-                <HomeCheckBoxIndicator>
+              <HomeCheckBox {...register("semana.terca.value")}>
+                <HomeCheckBoxIndicator >
                   <Check size={20} weight="bold" color="#fff" />
                 </HomeCheckBoxIndicator>
               </HomeCheckBox>
             </span>
             <span>
               <label>Qua</label>
-              <HomeCheckBox>
-                <HomeCheckBoxIndicator>
+              <HomeCheckBox {...register("semana.quarta.value")}>
+                <HomeCheckBoxIndicator >
                   <Check size={20} weight="bold" color="#fff" />
                 </HomeCheckBoxIndicator>
               </HomeCheckBox>
             </span>
             <span>
               <label>Qui</label>
-              <HomeCheckBox>
-                <HomeCheckBoxIndicator>
+              <HomeCheckBox {...register("semana.quinta.value")}>
+                <HomeCheckBoxIndicator  >
                   <Check size={20} weight="bold" color="#fff" />
                 </HomeCheckBoxIndicator>
               </HomeCheckBox>
             </span>
             <span>
               <label>Sex</label>
-              <HomeCheckBox>
-                <HomeCheckBoxIndicator>
+              <HomeCheckBox {...register("semana.sexta.value")}>
+                <HomeCheckBoxIndicator >
                   <Check size={20} weight="bold" color="#fff" />
                 </HomeCheckBoxIndicator>
               </HomeCheckBox>
             </span>
             <span>
               <label>Sab</label>
-              <HomeCheckBox>
-                <HomeCheckBoxIndicator>
+              <HomeCheckBox {...register("semana.sabado.value")}>
+                <HomeCheckBoxIndicator >
                   <Check size={20} weight="bold" color="#fff" />
                 </HomeCheckBoxIndicator>
               </HomeCheckBox>
@@ -160,23 +230,40 @@ export function ModalCreateNewClass({ name, closeModal }: ModalCreateNewClassPro
           <ModalCreateClassContentLines>
             <ModalCreateClassContentCollum>
               <label htmlFor="">Professor</label>
-              <select name="" id="">
-                <option value="">Selecione o professor</option>
+              <select id="" {...register("professor.id")}>
+                {teachers.map((teacher) => {
+                  return (
+                    <option key={teacher.id} value={teacher.id}>
+                      {teacher.nome}
+                    </option>
+                  );
+                })}
               </select>
             </ModalCreateClassContentCollum>
 
             <ModalCreateClassContentCollum>
               <label htmlFor="">Hora(s) por dia</label>
-              <input type="number" placeholder="Digite as horas" />
+              <input
+                {...register("cargaDiaria")}
+                type="number"
+                placeholder="Digite as horas"
+              />
             </ModalCreateClassContentCollum>
           </ModalCreateClassContentLines>
 
           <ModalCreateClassContentLine>
             <label htmlFor="">Ambiente</label>
-            <select placeholder="Digite o código da turma" >
-            {placesList.map((place) => {
-              return <option key={place.id}>{place.nome}</option>;
-            })}  
+            <select
+              {...register("ambiente.id")}
+              placeholder="Digite o código da turma"
+            >
+              {placesList.map((place) => {
+                return (
+                  <option value={place.id} key={place.id}>
+                    {place.nome}
+                  </option>
+                );
+              })}
             </select>
           </ModalCreateClassContentLine>
 
