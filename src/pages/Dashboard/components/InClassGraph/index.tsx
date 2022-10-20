152a9691ba901b2dcfc9454b8c7139079ab77440
Matheus Zacarias <matheuus412@gmail.com>
2022-10-20 20:37:42 -0300

    :sparkles: feat: Agora os professores são em aulas são exibidos

--- src/pages/Dashboard/components/InClassGraph/index.tsx
+++ src/pages/Dashboard/components/InClassGraph/index.tsx
@@ -1,29 +1,38 @@
+import { useEffect, useState } from "react";
+import { TeacherProps } from "../../../../Contexts/ObjectsContext";
+import { API } from "../../../../lib/axios";
 import { TeacherCard } from "../TeacherCard";
 import { InClassContainer, InclassTeacherCards } from "./style";
 
+export interface InclassTeacherProps {
+  professor: TeacherProps,
+  ambientes: string,
+  emAula: boolean,
+}
+
 export function InClassGraph() {
+  const [teacherInClass, setTeacherInClass] = useState<InclassTeacherProps[]>([])
+
+  async function fetchTeachersInClass() {
+    const res = await API.get("/professor/professorDisp/");
+    setTeacherInClass(res.data);
+  }
+
+  useEffect(() => {
+    fetchTeachersInClass()
+  },[])
+
     return (
-        
         <InClassContainer>
-     
             <h3>Em aula</h3>
-    
           <InclassTeacherCards>
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
-            <TeacherCard />
+            {
+              teacherInClass.map((teacher) => {
+                return(
+                  <TeacherCard key={teacher.professor.id} ambientes={teacher.ambientes} emAula={teacher.emAula} professor={teacher.professor} />
+                )
+              })
+            }
           </InclassTeacherCards>
         </InClassContainer>
     )
--- src/pages/Dashboard/components/TeacherCard/index.tsx
+++ src/pages/Dashboard/components/TeacherCard/index.tsx
@@ -1,17 +1,20 @@
+import { InclassTeacherProps } from "../InClassGraph";
 import { TeacherCardContainer, TeacherCardText } from "./style";
 
-export function TeacherCard() {
+
+
+export function TeacherCard({ professor, emAula, ambientes } : InclassTeacherProps) {
   return (
     <TeacherCardContainer>
       <TeacherCardText>
         <img src="https://avatars.githubusercontent.com/u/82879012?v=4" />
         <div>
-          <strong>Chile</strong>
-          <p>Laboratorio 15</p>
+          <strong>{professor.nome}</strong>
+          <p>{ambientes}</p>
         </div>
       </TeacherCardText>
 
-      <span>Em Aula</span>
+      <span>{emAula ? "Em Aula" : "Livre"}</span>
     </TeacherCardContainer>
   );
 }
